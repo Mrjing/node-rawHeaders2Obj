bcddd839d53f3319df4bf8842db1afd31cb879ab
Mrjing <837317210@qq.com>
2021-09-03 10:47:06 +0800

    fix: fix

--- index.js
+++ index.js
@@ -45,115 +45,115 @@
 */
 
 function matchKnownFields(field, lowercased) {
-    switch (field.length) {
-      case 3:
-        if (field === 'Age' || field === 'age') return 'age';
-        break;
-      case 4:
-        if (field === 'Host' || field === 'host') return 'host';
-        if (field === 'From' || field === 'from') return 'from';
-        if (field === 'ETag' || field === 'etag') return 'etag';
-        if (field === 'Date' || field === 'date') return '\u0000date';
-        if (field === 'Vary' || field === 'vary') return '\u0000vary';
-        break;
-      case 6:
-        if (field === 'Server' || field === 'server') return 'server';
-        if (field === 'Cookie' || field === 'cookie') return '\u0002cookie';
-        if (field === 'Origin' || field === 'origin') return '\u0000origin';
-        if (field === 'Expect' || field === 'expect') return '\u0000expect';
-        if (field === 'Accept' || field === 'accept') return '\u0000accept';
-        break;
-      case 7:
-        if (field === 'Referer' || field === 'referer') return 'referer';
-        if (field === 'Expires' || field === 'expires') return 'expires';
-        if (field === 'Upgrade' || field === 'upgrade') return '\u0000upgrade';
-        break;
-      case 8:
-        if (field === 'Location' || field === 'location')
-          return 'location';
-        if (field === 'If-Match' || field === 'if-match')
-          return '\u0000if-match';
-        break;
-      case 10:
-        if (field === 'User-Agent' || field === 'user-agent')
-          return 'user-agent';
-        if (field === 'Set-Cookie' || field === 'set-cookie')
-          return '\u0001';
-        if (field === 'Connection' || field === 'connection')
-          return '\u0000connection';
-        break;
-      case 11:
-        if (field === 'Retry-After' || field === 'retry-after')
-          return 'retry-after';
-        break;
-      case 12:
-        if (field === 'Content-Type' || field === 'content-type')
-          return 'content-type';
-        if (field === 'Max-Forwards' || field === 'max-forwards')
-          return 'max-forwards';
-        break;
-      case 13:
-        if (field === 'Authorization' || field === 'authorization')
-          return 'authorization';
-        if (field === 'Last-Modified' || field === 'last-modified')
-          return 'last-modified';
-        if (field === 'Cache-Control' || field === 'cache-control')
-          return '\u0000cache-control';
-        if (field === 'If-None-Match' || field === 'if-none-match')
-          return '\u0000if-none-match';
-        break;
-      case 14:
-        if (field === 'Content-Length' || field === 'content-length')
-          return 'content-length';
-        break;
-      case 15:
-        if (field === 'Accept-Encoding' || field === 'accept-encoding')
-          return '\u0000accept-encoding';
-        if (field === 'Accept-Language' || field === 'accept-language')
-          return '\u0000accept-language';
-        if (field === 'X-Forwarded-For' || field === 'x-forwarded-for')
-          return '\u0000x-forwarded-for';
-        break;
-      case 16:
-        if (field === 'Content-Encoding' || field === 'content-encoding')
-          return '\u0000content-encoding';
-        if (field === 'X-Forwarded-Host' || field === 'x-forwarded-host')
-          return '\u0000x-forwarded-host';
-        break;
-      case 17:
-        if (field === 'If-Modified-Since' || field === 'if-modified-since')
-          return 'if-modified-since';
-        if (field === 'Transfer-Encoding' || field === 'transfer-encoding')
-          return '\u0000transfer-encoding';
-        if (field === 'X-Forwarded-Proto' || field === 'x-forwarded-proto')
-          return '\u0000x-forwarded-proto';
-        break;
-      case 19:
-        if (field === 'Proxy-Authorization' || field === 'proxy-authorization')
-          return 'proxy-authorization';
-        if (field === 'If-Unmodified-Since' || field === 'if-unmodified-since')
-          return 'if-unmodified-since';
-        break;
-    }
-    if (lowercased) {
-      return '\u0000' + field;
-    }
-    return matchKnownFields(field.toLowerCase(), true);
+  switch (field.length) {
+    case 3:
+      if (field === 'Age' || field === 'age') return 'age';
+      break;
+    case 4:
+      if (field === 'Host' || field === 'host') return 'host';
+      if (field === 'From' || field === 'from') return 'from';
+      if (field === 'ETag' || field === 'etag') return 'etag';
+      if (field === 'Date' || field === 'date') return '\u0000date';
+      if (field === 'Vary' || field === 'vary') return '\u0000vary';
+      break;
+    case 6:
+      if (field === 'Server' || field === 'server') return 'server';
+      if (field === 'Cookie' || field === 'cookie') return '\u0002cookie';
+      if (field === 'Origin' || field === 'origin') return '\u0000origin';
+      if (field === 'Expect' || field === 'expect') return '\u0000expect';
+      if (field === 'Accept' || field === 'accept') return '\u0000accept';
+      break;
+    case 7:
+      if (field === 'Referer' || field === 'referer') return 'referer';
+      if (field === 'Expires' || field === 'expires') return 'expires';
+      if (field === 'Upgrade' || field === 'upgrade') return '\u0000upgrade';
+      break;
+    case 8:
+      if (field === 'Location' || field === 'location')
+        return 'location';
+      if (field === 'If-Match' || field === 'if-match')
+        return '\u0000if-match';
+      break;
+    case 10:
+      if (field === 'User-Agent' || field === 'user-agent')
+        return 'user-agent';
+      if (field === 'Set-Cookie' || field === 'set-cookie')
+        return '\u0001';
+      if (field === 'Connection' || field === 'connection')
+        return '\u0000connection';
+      break;
+    case 11:
+      if (field === 'Retry-After' || field === 'retry-after')
+        return 'retry-after';
+      break;
+    case 12:
+      if (field === 'Content-Type' || field === 'content-type')
+        return 'content-type';
+      if (field === 'Max-Forwards' || field === 'max-forwards')
+        return 'max-forwards';
+      break;
+    case 13:
+      if (field === 'Authorization' || field === 'authorization')
+        return 'authorization';
+      if (field === 'Last-Modified' || field === 'last-modified')
+        return 'last-modified';
+      if (field === 'Cache-Control' || field === 'cache-control')
+        return '\u0000cache-control';
+      if (field === 'If-None-Match' || field === 'if-none-match')
+        return '\u0000if-none-match';
+      break;
+    case 14:
+      if (field === 'Content-Length' || field === 'content-length')
+        return 'content-length';
+      break;
+    case 15:
+      if (field === 'Accept-Encoding' || field === 'accept-encoding')
+        return '\u0000accept-encoding';
+      if (field === 'Accept-Language' || field === 'accept-language')
+        return '\u0000accept-language';
+      if (field === 'X-Forwarded-For' || field === 'x-forwarded-for')
+        return '\u0000x-forwarded-for';
+      break;
+    case 16:
+      if (field === 'Content-Encoding' || field === 'content-encoding')
+        return '\u0000content-encoding';
+      if (field === 'X-Forwarded-Host' || field === 'x-forwarded-host')
+        return '\u0000x-forwarded-host';
+      break;
+    case 17:
+      if (field === 'If-Modified-Since' || field === 'if-modified-since')
+        return 'if-modified-since';
+      if (field === 'Transfer-Encoding' || field === 'transfer-encoding')
+        return '\u0000transfer-encoding';
+      if (field === 'X-Forwarded-Proto' || field === 'x-forwarded-proto')
+        return '\u0000x-forwarded-proto';
+      break;
+    case 19:
+      if (field === 'Proxy-Authorization' || field === 'proxy-authorization')
+        return 'proxy-authorization';
+      if (field === 'If-Unmodified-Since' || field === 'if-unmodified-since')
+        return 'if-unmodified-since';
+      break;
+  }
+  if (lowercased) {
+    return '\u0000' + field;
+  }
+  return matchKnownFields(field.toLowerCase(), true);
 }
 
 
-function getObjFromRawHeaders(rawHeaders)  {
-    const headersMap = {}
-    const n = rawHeaders.length
-    for(let i = 0;i < n; i+=2) {
-        addHeaderLine(rawHeaders[i], rawHeaders[i+1], headersMap)
-    }
-    return headersMap
+function getObjFromRawHeaders(rawHeaders) {
+  const headersMap = {}
+  const n = rawHeaders.length
+  for (let i = 0; i < n; i += 2) {
+    addHeaderLine(rawHeaders[i], rawHeaders[i + 1], headersMap)
+  }
+  return headersMap
 }
 
-function addHeaderLine(field, value ,dest) {
-    let transformField = matchKnownFields(field);
-    const flag = transformField.charCodeAt(0);
+function addHeaderLine(field, value, dest) {
+  let transformField = matchKnownFields(field);
+  const flag = transformField.charCodeAt(0);
   if (flag === 0 || flag === 2) {
     transformField = transformField.slice(1);
     // Make a delimited list
@@ -164,10 +164,10 @@ function addHeaderLine(field, value ,dest) {
     }
   } else if (flag === 1) {
     // Array header -- only Set-Cookie at the moment
-    if (dest['set-cookie'] !== undefined) {
-      dest['set-cookie'].push(value);
+    if (dest[field] !== undefined) {
+      dest[field].push(value);
     } else {
-      dest['set-cookie'] = [value];
+      dest[field] = [value];
     }
   } else if (dest[field] === undefined) {
     // Drop duplicates
@@ -176,5 +176,5 @@ function addHeaderLine(field, value ,dest) {
 }
 
 module.exports = {
-    getObjFromRawHeaders
+  getObjFromRawHeaders
 }
--- test.js
+++ test.js
@@ -1,29 +1,29 @@
 const test = require('tape')
-const {getObjFromRawHeaders} = require('./index')
+const { getObjFromRawHeaders } = require('./index')
 
 test('simple', function (t) {
     const rawHeaders = [
-        'Server',         'Github.com',
-        'Server',         'Gitlab.com',
-        'A',              'c',
-        'A',              'd',
-        'date',           'Tue',
-        'date',           'Fri',
-        'cookie',         'a=b; b=c',
-        'set-cookie',     'a=b',
-        'set-cookie',     'b=c',
-        'referer',        'https://github.com',
-        'referer',        'https://gitlab.com',
-        'Connection',     'close',
+        'Server', 'Github.com',
+        'Server', 'Gitlab.com',
+        'A', 'c',
+        'A', 'd',
+        'date', 'Tue',
+        'date', 'Fri',
+        'cookie', 'a=b; b=c',
+        'Set-Cookie', 'a=b',
+        'Set-Cookie', 'b=c',
+        'referer', 'https://github.com',
+        'referer', 'https://gitlab.com',
+        'Connection', 'close',
         'Content-Length', '2'
-      ]
+    ]
 
     const rawHeadersObj = {
         Server: 'Github.com',
         A: 'c, d',
         date: 'Tue, Fri',
         cookie: 'a=b; b=c',
-        'set-cookie': [ 'a=b', 'b=c' ],
+        'Set-Cookie': ['a=b', 'b=c'],
         referer: 'https://github.com',
         Connection: 'close',
         'Content-Length': '2'
